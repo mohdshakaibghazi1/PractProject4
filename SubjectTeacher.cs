12f283b7ab49d7bcb2ccafc36367deb0c5b7db02
Mohd Shakaib Ghazi <mohd.shakaibghazi@mphasis.com>
2023-08-03 15:11:41 +0530

    pp4

---
+++ SubjectTeacher.cs
@@ -0,0 +1,21 @@
+﻿using Project4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4
+{
+    public class TeacherWithSubjects
+    {
+        public Teacher Teacher { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public TeacherWithSubjects()
+        {
+            Subjects = new List<Subject>();
+        }
+    }
+
+}
